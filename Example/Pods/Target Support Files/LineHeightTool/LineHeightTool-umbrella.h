769ebd9d7c8b9f02601e09e75558f7f30fa65249
Maheep Kaushal <maheepk@Maheeps-MacBook-Pro.local>
2015-09-30 17:32:39 +0530

    Intial Commit

--- Example/Pods/Target Support Files/LineHeightTool/LineHeightTool-umbrella.h
+++ Example/Pods/Target Support Files/LineHeightTool/LineHeightTool-umbrella.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+#import "LineHeightConstant.h"
+#import "NSString+Height.h"
+#import "UILabelWithFexibleHeight.h"
 
 FOUNDATION_EXPORT double LineHeightToolVersionNumber;
 FOUNDATION_EXPORT const unsigned char LineHeightToolVersionString[];
